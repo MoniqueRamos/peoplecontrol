ec7222f4ab147897b4da3b7e0121920c131bb63b
Monique Ramos <moniqueloly@hotmail.com>
2023-01-15 21:36:20 -0300

    Tornando o endereco principal como unico

--- src/main/java/com/api/peoplecontrol/services/EnderecoService.java
+++ src/main/java/com/api/peoplecontrol/services/EnderecoService.java
@@ -4,9 +4,12 @@ import com.api.peoplecontrol.models.Endereco;
 import com.api.peoplecontrol.models.Pessoa;
 import com.api.peoplecontrol.repositories.EnderecoRepository;
 import com.api.peoplecontrol.repositories.PessoaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,6 +30,12 @@ public class EnderecoService {
         Optional<Pessoa> pessoa =  pessoaRepository.findById(id);
         if(pessoa.isPresent()) {
             endereco.setPessoa(pessoa.get());
+            List<Endereco> enderecos = enderecoRepository.findAllByPessoaId(id);
+            if(endereco.getPrincipal()==true){
+                for(Endereco end:enderecos){
+                    end.setPrincipal(false);
+                }
+        }
         }
         return enderecoRepository.save(endereco);
     }
